34b4b0d0061d6f2a10375a0e882fe786ebf4c91e
akash <akshdhr117@gmail.com>
2025-11-28 23:29:57 +0530

    Very important BS concept. Please revisit.

--- Leetcode/1539_kth_missing_positive_number.py
+++ Leetcode/1539_kth_missing_positive_number.py
@@ -13,3 +13,16 @@ class Solution:
 # Trick is to find the number of missing numbers before a particular index
 # This can be done using the formula: missing_before_i = arr[i] - (i+1) as (i+1) is the true value for the ith index.
 # To revisit this problem again
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        low = 0
+        high = len(arr) - 1
+        # This can be done using the formula: missing_before_i = arr[i] - (i+1) as (i+1) is the true value for the ith index.
+        while low <= high:
+            mid = (low + high) // 2
+            if arr[mid] -(mid+1) < k:
+                low = mid + 1
+            else:
+                high = mid - 1
+        # After the binary search high will be pointing to the starting range of missing number and low to the ending range
+        return high + k + 1
