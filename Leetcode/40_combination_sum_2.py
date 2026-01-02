09d2743487241eddadbcf5755f43d7f94472e9ca
akash <akshdhr117@gmail.com>
2026-01-02 21:40:48 +0530

    Very IMP problem. Need to revisit.

---
+++ Leetcode/40_combination_sum_2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        ds = []
+        candidates.sort()
+        def find_combination(candidates, res, ds, ind, target):
+            if target == 0:
+                res.append(ds.copy())
+                return
+            if ind == len(candidates) or target < 0:
+                return
+            
+            ds.append(candidates[ind])
+            find_combination(candidates, res, ds, ind+1, target - candidates[ind])
+            ds.pop()
+            next_ind = ind + 1
+
+            while next_ind < len(candidates) and candidates[next_ind] == candidates[ind]:
+                next_ind += 1
+
+            find_combination(candidates, res, ds, next_ind, target)
+        find_combination(candidates, res, ds, 0, target)
+        return res
