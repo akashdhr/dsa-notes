8e63f065d3a72e9bce4c58985a0ae7bae5072dca
akash <akshdhr117@gmail.com>
2026-01-04 11:26:23 +0530

    need to revisit - very interesting recursion problem

---
+++ Leetcode/51_N_Queens.py
@@ -0,0 +1,42 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [['.' for _ in range(n)] for _ in range(n)]
+        ans = []
+        # col = column index
+        def solve(col, board, n, ans):
+            if col == n:
+                temp = [''.join(i) for i in board]
+                ans.append(temp)
+                return
+            for row in range(0, n):
+                if safe(row, col, board, n):
+                    board[row][col] = 'Q'
+                    solve(col + 1, board, n, ans)
+                    board[row][col] = '.'
+        def safe(row, col, board, n):
+            # Upper diagonal
+            rowc = row
+            colc = col
+            while rowc>=0 and colc>=0:
+                if board[rowc][colc] == 'Q':
+                    return False
+                rowc -= 1
+                colc -= 1
+            # Left row
+            rowc = row
+            colc = col
+            while colc>=0:
+                if board[rowc][colc] == 'Q':
+                    return False
+                colc -= 1
+            # Lower diagonal
+            rowc = row
+            colc = col
+            while rowc<n and colc>=0:
+                if board[rowc][colc] == 'Q':
+                    return False
+                rowc += 1
+                colc -= 1
+            return True
+        solve(0, board, n, ans)
+        return ans
