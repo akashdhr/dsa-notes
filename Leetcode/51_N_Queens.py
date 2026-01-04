74b11b1fe7ec233a4a8c14bb010a3382f3957127
akash <akshdhr117@gmail.com>
2026-01-04 12:33:34 +0530

    optimised approach

--- Leetcode/51_N_Queens.py
+++ Leetcode/51_N_Queens.py
@@ -1,3 +1,4 @@
+# Time complexity: O(N!*N)
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         board = [['.' for _ in range(n)] for _ in range(n)]
@@ -38,5 +39,37 @@ class Solution:
                 rowc += 1
                 colc -= 1
             return True
+        solve(0, board, n, ans)
+        return ans
+
+## Optimised approach
+# Time complexity: O(N!)
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [['.' for _ in range(n)] for _ in range(n)]
+        ans = []
+        rows = set()
+        upper_diag = set() # row - col
+        lower_diag = set() # row + col
+        # col = column index
+        def solve(col, board, n, ans):
+            if col == n:
+                temp = [''.join(i) for i in board]
+                ans.append(temp)
+                return
+            for row in range(n):
+                if row in rows or (row - col) in upper_diag or (row+col) in lower_diag:
+                    continue
+                board[row][col] = 'Q'
+                rows.add(row)
+                upper_diag.add(row-col)
+                lower_diag.add(row+col)
+                solve(col+1, board, n, ans)
+                board[row][col] = '.'
+                rows.remove(row)
+                upper_diag.remove(row-col)
+                lower_diag.remove(row+col)
+            return
+        
         solve(0, board, n, ans)
         return ans
