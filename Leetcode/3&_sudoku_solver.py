69d7c316e561c71cedcbf0e28b3bdc24354f24ae
akash <akshdhr117@gmail.com>
2026-01-04 22:47:55 +0530

    not optimal but works. need to revisit.

---
+++ Leetcode/3&_sudoku_solver.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        def isValid(row, col, num, board):
+            # Check row
+            for c in range(9):
+                if board[row][c] == num:
+                    return False
+            # Check col
+            for r in range(9):
+                if board[r][col] == num:
+                    return False
+            base_row = (row // 3) * 3
+            base_col = (col // 3) * 3
+            # Check box
+            for r in range(base_row, base_row + 3):
+                for c in range(base_col, base_col + 3):
+                    if board[r][c] == num:
+                        return False
+            return True
+
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == '.':
+                    for num in map(str, range(1, 10)):
+                        if isValid(row, col, num, board):
+                            board[row][col] = num
+                            if self.solveSudoku(board):
+                                return True
+                            else:
+                                board[row][col] = '.'
+                    return False
+        return True            
