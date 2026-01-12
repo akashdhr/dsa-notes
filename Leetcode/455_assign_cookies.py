83cbbaafaa5e98a063ba2efe932136b56ce3853d
akash <akshdhr117@gmail.com>
2026-01-12 23:36:22 +0530

    updated complexity

--- Leetcode/455_assign_cookies.py
+++ Leetcode/455_assign_cookies.py
@@ -13,6 +13,7 @@ class Solution:
                 gp += 1
             sp += 1
         return cnt
-
+ # time complexity: O(N log N + M log M) where N is the number of children and M is the number of cookies.
+ # space complexity: O(1)
 
         
