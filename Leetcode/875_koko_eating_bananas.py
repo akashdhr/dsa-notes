d90d1bb058ce0ebf057367c47df0ea75cf4df167
akash <akshdhr117@gmail.com>
2026-01-17 23:42:22 +0530

    revisit

--- Leetcode/875_koko_eating_bananas.py
+++ Leetcode/875_koko_eating_bananas.py
@@ -13,7 +13,7 @@ class Solution:
         # if pile[i] < k all bananas eaten and no more will be eaten the same hour
         
         high = max(piles) # Speed cannot be more than this value
-        low = 1
+        low = 1 # why? because min speed can be 1 banana/hr
         res = -1
         while low <= high:
             mid = (low+high)//2
@@ -24,6 +24,8 @@ class Solution:
             else:
                 low = mid + 1
         return res
-            
+
+# time complexity: O(N log M) where N is the number of piles and M is the maximum bananas in a pile. why? because for each speed we are calculating total hours which takes O(N) time and we are doing binary search on speed which takes O(log M) time.
+# space complexity: O(1) as we are using only constant extra space.       
 
         
