f98ea88639150f15141c81114e2e155b54462bca
akash <akshdhr117@gmail.com>
2026-01-25 10:39:13 +0530

    very good DFS problem

---
+++ Leetcode/662_maximum_width_binary_tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Using BFS approach
+# time complexity: O(N) where N is the number of nodes in the binary tree. We visit each node exactly once.
+# space complexity: O(M) where M is the maximum number of nodes at any level in the binary tree. In the worst case, this can be O(N/2) = O(N) for a balanced binary tree.
+from collections import deque
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        q = deque([[root, 1, 0]]) # [node, num, level]
+        prevLevel, prevNum = 0, 1
+        while q:
+            node, num, level = q.popleft()
+
+            if level > prevLevel:
+                prevLevel = level
+                prevNum = num
+
+            res = max(res, num - prevNum + 1)
+            if node.left:
+                q.append([node.left, 2*num, level + 1])
+            if node.right:
+                q.append([node.right, 2*num + 1, level + 1])
+            
+        return res
