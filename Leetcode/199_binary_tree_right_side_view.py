2587e1c1eee56ffc5a7cbeb16910da17af54c733
akash <akshdhr117@gmail.com>
2026-01-26 15:33:54 +0530

    bfs approach

--- Leetcode/199_binary_tree_right_side_view.py
+++ Leetcode/199_binary_tree_right_side_view.py
@@ -19,3 +19,31 @@ class Solution:
             
 # time complexity: O(N) where N is the number of nodes in the binary tree. We visit each node exactly once.
 # space complexity: O(H) where H is the height of the binary tree. This space is used by the recursion stack. In the worst case (skewed tree), H can be O(N). In a balanced tree, H is O(log N).
+
+# BFS approach
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        q = deque([root])
+        if not root:
+            return []
+        res = []
+        while q:
+            size = len(q)
+            for i in range(size):
+                node = q.popleft()
+                if i == size - 1:
+                    res.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return res
+# time complexity: O(N) where N is the number of nodes in the binary tree. We visit each node exactly once.
+# space complexity: O(W) where W is the maximum width of the binary tree. This
