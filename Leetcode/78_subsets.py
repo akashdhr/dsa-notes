954b4b4f316d491c2819ab8ec61e7af0fc90e976
akash <akshdhr117@gmail.com>
2026-01-29 22:02:43 +0530

    revision needed

--- Leetcode/78_subsets.py
+++ Leetcode/78_subsets.py
@@ -13,4 +13,5 @@ class Solution:
         subset(nums, res, ds, 0)
         return res
 
-        
+       #    time complexity: O(N * 2^N) where N is the length of nums. In the worst case, we generate all subsets (2^N) and copying each subset takes O(N) time.
+       #    space complexity: O(N) for the recursion stack and the temporary list used to store subsets. 
--- Leetcode/90_subsets_2.py
+++ Leetcode/90_subsets_2.py
@@ -1,18 +1,19 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        ds = []
         nums.sort()
-        def subset(nums, res, ds, ind):
+        res = []
+        def dfs(ind, ds):
             if ind == len(nums):
                 res.append(ds.copy())
                 return
             ds.append(nums[ind])
-            subset(nums, res, ds, ind+1)
+            dfs(ind + 1, ds)
             ds.pop()
             next_ind = ind + 1
             while next_ind < len(nums) and nums[next_ind] == nums[ind]:
                 next_ind += 1
-            subset(nums, res, ds, next_ind)
-        subset(nums, res, ds, 0)
+            dfs(next_ind, ds)
+        dfs(0, [])
         return res
+# time complexity: O(N * 2^N) where N is the length of nums. In the worst case, we generate all subsets (2^N) and copying each subset takes O(N) time.
+# space complexity: O(N) for the recursion stack and the temporary list used to store subsets
