1a312231ad2d9f06a907ba8504f80b658535aa4a
akash <akshdhr117@gmail.com>
2026-01-31 10:02:30 +0530

    optimal solution

--- Leetcode/77_combinations.py
+++ Leetcode/77_combinations.py
@@ -16,5 +16,22 @@ class Solution:
             dfs(ind + 1, ds)
         dfs(0, [])
         return res
-#time complexity: O(C(n, k) * k) where C(n, k) is the number of combinations and k is the time taken to copy each combination to the result list.
+#time complexity: So the real complexity is: O(2ⁿ + C(n, k) * k) But in interviews we usually say: O(2ⁿ) because that dominates.
 #space complexity: O(k) for the recursion stack and the temporary list used to store combinations.
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        def dfs(ind, ds):
+            if len(ds) == k:
+                res.append(ds.copy())
+                return
+            for i in range(ind, n+1):
+                ds.append(i)
+                dfs(i+1, ds)
+                ds.pop()
+        dfs(1, [])
+        return res        
+    
+# time complexity: O(C(n, k) * k)
+# space complexity: O(C(n, k) * k)
