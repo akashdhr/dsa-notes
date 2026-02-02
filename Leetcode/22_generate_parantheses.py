06cee24cccc176691bac2490565bd0639c032a8b
akash <akshdhr117@gmail.com>
2026-02-02 21:56:50 +0530

    very good backtracking problem

---
+++ Leetcode/22_generate_parantheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        stack = []
+        def backtracking(o, c):
+            if o == c == n:
+                res.append(''.join(stack))
+                return
+            if o < n:
+                stack.append('(')
+                backtracking(o+1, c)
+                stack.pop()
+            if c < o:
+                stack.append(')')
+                backtracking(o, c+1)
+                stack.pop()
+        backtracking(0, 0)
+        return res
+        
+# time complexity: O(4^n / sqrt(n)) where n is the number of pairs of parentheses.
+# space Complexity: O(4^n / sqrt(n)) to store all valid combinations.
