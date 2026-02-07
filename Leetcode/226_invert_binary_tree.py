051af6ed1e4d4b5672f6d88e10884a8b4b9de1a2
akash <akshdhr117@gmail.com>
2026-02-07 15:35:34 +0530

    revision

--- Leetcode/226_invert_binary_tree.py
+++ Leetcode/226_invert_binary_tree.py
@@ -6,11 +6,13 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root:
-            return
-        root.left, root.right = root.right, root.left
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+        def dfs(node):
+            if not node:
+                return
+            node.left, node.right = node.right, node.left
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
         return root
 #time complexity: O(N) where N is the number of nodes in the binary tree. We visit each node exactly once.
 #space complexity: O(H) where H is the height of the binary tree. This space is used by the recursion stack. In the worst case (skewed tree), H can be O(N). In a balanced tree, H is O(log N).
