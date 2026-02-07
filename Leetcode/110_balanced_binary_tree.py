2c5350d258cf326e0d94eb7d99e51259259f050c
akash <akshdhr117@gmail.com>
2026-02-07 18:06:24 +0530

    revision

--- Leetcode/110_balanced_binary_tree.py
+++ Leetcode/110_balanced_binary_tree.py
@@ -37,21 +37,16 @@ class Solution:
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def check(node):
+        def dfs(node):
             if not node:
                 return 0
-            left = check(node.left)
-            right = check(node.right)
-
-            if left == -1 or right == -1:
+            l = dfs(node.left)
+            r = dfs(node.right)
+            if l == -1 or r == -1:
                 return -1
-
-            if abs(left-right) > 1:
+            if abs(l - r) > 1:
                 return -1
-
-            return 1 + max(left, right)
-        if check(root) == -1:
-            return False
-        return True
+            return 1 + max(l, r)
+        return dfs(root) != -1
 #time complexity: O(N) where N is the number of nodes in the binary tree. We visit each node exactly once.
 #space complexity: O(H) where H is the height of the binary tree. This space is used by the recursion stack. In the worst case (skewed tree), H can be O(N). In a balanced tree, H is O(log N).
