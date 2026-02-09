cad948825a4b26572ecd0780e1382a941b95135d
akash <akshdhr117@gmail.com>
2026-02-09 20:34:38 +0530

    was not able to solve on my own

---
+++ Leetcode/98_validate_BST.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node, mini, maxi):
+            if not node:
+                return True
+            if not (mini < node.val < maxi):
+                return False
+            else:
+                return dfs(node.left, mini, node.val) and dfs(node.right, node.val, maxi)
+        return dfs(root, float('-inf'), float('inf'))
+#time complexity: O(N) where N is the number of nodes in the binary tree. We visit each node exactly once.
+#space complexity: O(H) where H is the height of the binary tree.
