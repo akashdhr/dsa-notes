358e624cf0ffe8ab0ec033814271bc39aa0bd5fc
akash <akshdhr117@gmail.com>
2026-02-12 22:15:59 +0530

    revision

--- Leetcode/22_generate_parantheses.py
+++ Leetcode/22_generate_parantheses.py
@@ -1,21 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
-        stack = []
-        def backtracking(o, c):
-            if o == c == n:
-                res.append(''.join(stack))
+        def dfs(arr, s, c):
+            if s == c == n:
+                res.append(''.join(arr.copy()))
                 return
-            if o < n:
-                stack.append('(')
-                backtracking(o+1, c)
-                stack.pop()
-            if c < o:
-                stack.append(')')
-                backtracking(o, c+1)
-                stack.pop()
-        backtracking(0, 0)
+            if s < n:
+                arr.append('(')
+                dfs(arr, s+1, c)
+                arr.pop()
+            if c < s:
+                arr.append(')')
+                dfs(arr, s, c+1)
+                arr.pop()
+        dfs([], 0, 0)
         return res
+        
         
 # time complexity: O(4^n / sqrt(n)) where n is the number of pairs of parentheses.
 # space Complexity: O(4^n / sqrt(n)) to store all valid combinations. 
