f8b282d1248b81ef134af4d3e340fff3031e694d
akash <akshdhr117@gmail.com>
2026-02-21 19:42:52 +0530

    revision

--- Leetcode/875_koko_eating_bananas.py
+++ Leetcode/875_koko_eating_bananas.py
@@ -1,30 +1,22 @@
-import math
-
 class Solution:
-    def totalHrs(self, piles, speed):
-        hrs = 0
-        for i in piles:
-            hrs += math.ceil(i/speed)
-        return hrs
-            
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # ith pile has piles[i] bananas
-        # h = hours; k = bananas/hr speed
-        # if pile[i] < k all bananas eaten and no more will be eaten the same hour
+        def totalTime(piles, speed):
+            t = 0
+            for i in piles:
+                t += math.ceil(i/speed)
+            return t
         
-        high = max(piles) # Speed cannot be more than this value why?
-        low = 1 # why? because min speed can be 1 banana/hr
+        high = max(piles) # speed cannot be more than max bananas/pile in piles
+        low = 1
         res = -1
         while low <= high:
-            mid = (low+high)//2
-            # check if Koko can finish with speed = mid
-            if self.totalHrs(piles, mid) <= h:
+            mid = (low + high)//2
+            if totalTime(piles, mid) <= h:
                 res = mid
                 high = mid - 1
             else:
                 low = mid + 1
         return res
-
 
 
 # time complexity: O(N log M) where N is the number of piles and M is the maximum bananas in a pile. why? because for each speed we are calculating total hours which takes O(N) time and we are doing binary search on speed which takes O(log M) time.
