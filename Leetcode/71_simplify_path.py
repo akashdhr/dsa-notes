46bed03012e09a0f9f39640dd7cfa34fbffe5c8b
akash <akshdhr117@gmail.com>
2026-02-22 19:32:18 +0530

    revision

--- Leetcode/71_simplify_path.py
+++ Leetcode/71_simplify_path.py
@@ -1,19 +1,16 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        # Start by splitting the path based on / slashes
+        stack = []
         pathArr = path.split('/')
-        res = []
-        # Base cases check for ignoring
         for i in pathArr:
-            if i == '' or i == '.':
+            if stack and i == '..':
+                stack.pop()
+            elif i == '..' or i == '.':
                 continue
-            elif i == '..':
-                if len(res) > 0:
-                    res.pop()
-            else:
-                res.append(i)
-        res = '/' + '/'.join(res)
-        return res
-# time complexity: O(N) where N is the length of the path.
-# space complexity: O(N) in the worst case, we may store all the directories in the stack.
+            elif i != '':
+                stack.append(i)
+        return ('/' + '/'.join(stack))
+        
+# time complexity: O(N) where N is the length of the input path string, since we need to iterate through the path string once to split it and then again to process each component.
+# space complexity: O(N) in the worst case, if the input path contains many valid directory names, we may end up storing all of them in the stack.
         
