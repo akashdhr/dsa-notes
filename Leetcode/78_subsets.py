18af5be8555c3fef737cbe71ad7559c8bdf2daa6
akash <akshdhr117@gmail.com>
2026-02-28 22:13:36 +0530

    revision

--- Leetcode/78_subsets.py
+++ Leetcode/78_subsets.py
@@ -1,16 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        ds = []
-        def subset(nums, res, ds, ind):
+        def dfs(ind, ds):
             if ind == len(nums):
                 res.append(ds.copy())
                 return
             ds.append(nums[ind])
-            subset(nums, res, ds, ind+1)
+            dfs(ind+1, ds)
             ds.pop()
-            subset(nums, res, ds, ind+1)
-        subset(nums, res, ds, 0)
+            dfs(ind+1, ds)
+        dfs(0, [])
         return res
 
        #    time complexity: O(N * 2^N) where N is the length of nums. In the worst case, we generate all subsets (2^N) and copying each subset takes O(N) time.
