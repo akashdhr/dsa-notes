756d33d02e24ee0003089c73eaf68b26253a247c
akash <akshdhr117@gmail.com>
2026-03-06 18:53:05 +0530

    revision

--- Leetcode/217_contains_duplicate.py
+++ Leetcode/217_contains_duplicate.py
@@ -1,10 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hset = {}
+        hashset = set()
         for i in nums:
-            hset[i] = hset.get(i, 0) + 1
-        for i in hset.values():
-            if i > 1:
+            if i in hashset:
                 return True
+            else:
+                hashset.add(i)
         return False
-        
+
+# time complexity: O(N) where N is the number of elements in the input array. We traverse the array once and each lookup and insertion operation in the hash set takes O(1) time on average.
+# space complexity: O(N) in the worst case when all elements in the array are distinct, we will have to store all elements in the hash set.
