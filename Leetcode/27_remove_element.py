c058fc0b8073b07bd6b044f1d426c5ee9318c645
akash <akshdhr117@gmail.com>
2026-03-11 22:20:14 +0530

    revision

--- Leetcode/27_remove_element.py
+++ Leetcode/27_remove_element.py
@@ -1,16 +1,15 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        slow = 0
-        fast = 1
-        # find first index where i = val
-        while slow < len(nums):
-            if nums[slow] == val:
+        first = len(nums)
+        for i in range(len(nums)):
+            if nums[i] == val:
+                first = i
                 break
-            slow += 1
-        for i in range(slow+1, len(nums)):
-            if nums[i] != val:
-                nums[slow], nums[i] = nums[i], nums[slow]
-                slow += 1
-        return slow
-# time complexity: O(N)
-# space complexity: O(1)
+        for j in range(first+1, len(nums)):
+            if nums[j] != val:
+                nums[j], nums[first] = nums[first], nums[j]
+                first += 1
+        return first
+
+# time complexity: O(N) where N is the length of the input array. We traverse the array once to find the first occurrence of val and then traverse it again to move the non-val elements to the left.
+# space complexity: O(1) since we are using only a constant amount of extra space to store the index of the first occurrence of val and to swap elements in place.
