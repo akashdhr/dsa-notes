e974dbefe2709e871a17f32f99c576ce5f397462
akash <akshdhr117@gmail.com>
2026-05-01 12:37:44 +0530

    revision

--- Leetcode/1_two_sum.py
+++ Leetcode/1_two_sum.py
@@ -7,5 +7,6 @@ class Solution:
                 return [store[diff], i]
             else:
                 store[v] = i
+                
 # time complexity: O(N) where N is the number of elements in the input array. We traverse the array once and each lookup and insertion operation in the hash map takes O(1) time on average.
 # space complexity: O(N) in the worst case when all elements in the array are distinct.
