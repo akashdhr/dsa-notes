d9a5428f0d134fd4950a5fc9eb82b61dbee01e3f
akash <akshdhr117@gmail.com>
2026-05-01 12:21:45 +0530

    revision

--- Leetcode/217_contains_duplicate.py
+++ Leetcode/217_contains_duplicate.py
@@ -7,6 +7,5 @@ class Solution:
             else:
                 hashset.add(i)
         return False
-
 # time complexity: O(N) where N is the number of elements in the input array. We traverse the array once and each lookup and insertion operation in the hash set takes O(1) time on average.
 # space complexity: O(N) in the worst case when all elements in the array are distinct, we will have to store all elements in the hash set.
