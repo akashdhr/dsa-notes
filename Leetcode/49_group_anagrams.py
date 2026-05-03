95504514100d7034c5c083e5c28ea1ec16e7879d
akash <akshdhr117@gmail.com>
2026-05-03 08:22:09 +0530

    revision

--- Leetcode/49_group_anagrams.py
+++ Leetcode/49_group_anagrams.py
@@ -9,6 +9,5 @@ class Solution:
                 anagrams[temp] = [s]
         return list(anagrams.values())
         
-        
 # time complexity: O(N * K log K) where N is the number of strings in the input array and K is the maximum length of a string in the input array. Sorting each string takes O(K log K) time and we do this for all N strings.
 # space complexity: O(N * K) in the worst case when all strings are anagrams of each other, we will have to store all strings in the hash map.
