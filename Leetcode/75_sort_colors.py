fad573af1025cb39ab993f0e75e9694fff409ce9
akash <akshdhr117@gmail.com>
2026-05-05 23:38:46 +0530

    revision

--- Leetcode/75_sort_colors.py
+++ Leetcode/75_sort_colors.py
@@ -32,18 +32,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        low = med = 0
-        high = len(nums)-1
-        while med <= high:
-            if nums[med] == 2:
-                nums[med], nums[high] = nums[high], nums[med]
+        low = mid = 0
+        high = len(nums) - 1
+        while mid <= high:
+            if nums[mid] == 2:
+                nums[mid], nums[high] = nums[high], nums[mid]
                 high -= 1
-            elif nums[med] == 0:
-                nums[low], nums[med] = nums[med], nums[low]
+            elif nums[mid] == 0:
+                nums[mid], nums[low] = nums[low], nums[mid]
                 low += 1
-                med += 1
+                mid += 1
             else:
-                med += 1
+                mid += 1  
         return nums
+        
         
 
