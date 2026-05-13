79cb241bfa3332dc84ee3793a9824f185adbb483
akash <akshdhr117@gmail.com>
2026-05-13 23:31:41 +0530

    revision

--- Leetcode/1768_merge_strings.py
+++ Leetcode/1768_merge_strings.py
@@ -1,18 +1,19 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        w1 = 0
-        w2 = 0
+        w1 = w2 = 0
         res = ''
-        while w1 < len(word1) and w2 < len(word2):
+        minLen = min(len(word1), len(word2))
+        for i in range(minLen):
             res += word1[w1]
             res += word2[w2]
             w1 += 1
             w2 += 1
-        if w1 < len(word1):
+        if w1 != len(word1):
             res += word1[w1:]
-        if w2 < len(word2):
+        if w2 != len(word2):
             res += word2[w2:]
         return res
-
+        
+    
 # time complexity: O(N + M) where N and M are the lengths of word1 and word2 respectively.
 # space complexity: O(N + M) for the result string.
