67460c0329d3845bfba554cb8a37876eba439a05
akash <akshdhr117@gmail.com>
2026-07-27 23:44:20 +0530

    revision

--- Leetcode/242_valid_anagram.py
+++ Leetcode/242_valid_anagram.py
@@ -1,14 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        seen = {}
+        seen = dict()
         for i in s:
             seen[i] = seen.get(i, 0) + 1
-        for j in t:
-            seen[j] = seen.get(j, 0) - 1
-        for v in seen.values():
-            if v != 0:
+        for i in t:
+            if i not in seen:
+                return False
+            else:
+                seen[i] -= 1
+        for i in seen.values():
+            if i != 0:
                 return False
         return True
+        
 
 # time complexity: O(N) where N is the length of the input strings. We traverse both strings once, and each lookup and update operation in the hash map takes O(1) time on average.
 # space complexity: O(1) since the hash map will contain at most 26 keys (assuming only lowercase English letters), which is a constant amount of space.
