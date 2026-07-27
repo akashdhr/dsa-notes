abb47eefd561e0cb62e12defa84686cb3cfc36da
akash <akshdhr117@gmail.com>
2026-07-27 23:57:13 +0530

    revision

--- Leetcode/121_best_stock.py
+++ Leetcode/121_best_stock.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        low = 0
-        high = 1
-        profit = 0
-        while high < len(prices):
-            diff = prices[high] - prices[low]
-            if diff <= 0:
-                low = high
-            elif diff > 0:
-                profit = max(profit, diff)
-            high +=1
-        return profit
+        l = 0
+        r = 1
+        maxD = 0
+        while r < len(prices):
+            diff = prices[r] - prices[l]
+            if diff < 0:
+                l = r
+            r += 1
+            maxD = max(diff, maxD)
+        return maxD
 
 # time complexity: O(N) where N is the number of days (length of the prices array) since we traverse the array once.
 # space complexity: O(1) since we are using only a constant amount of extra space to store the pointers and the profit variable.
