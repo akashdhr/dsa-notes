d599d69a587e970a80d5d09d99fea5205c4fa718
akash <akshdhr117@gmail.com>
2026-07-28 23:26:11 +0530

    revision

--- Leetcode/347_top_k_frequent_elements.py
+++ Leetcode/347_top_k_frequent_elements.py
@@ -1,18 +1,20 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        for i in range(len(nums)):
-            count[nums[i]] = count.get(nums[i], 0) + 1
+        store = {}
+        for i in nums:
+            store[i] = store.get(i, 0) + 1
         freq = []
-        for i,j in count.items():
-            temp = [j, i]
-            freq.append(temp)
+        for i,v in store.items():
+            freq.append([v,i])
         freq.sort()
-        res = []
+        ans = []
+        print(freq)
         while k > 0:
-            res.append(freq.pop()[1])
+            ans.append(freq.pop()[1])
             k -= 1
-        return res
+        return ans
+
+        
         
 # time complexity: O(N log N) where N is the number of unique elements in the input array. We count the frequency of each element in O(N) time, then we sort the frequency list which takes O(N log N) time, and finally we retrieve the top k elements which takes O(k) time.
 # space complexity: O(N) where N is the number of unique elements in the input array since we are using a hash map to store the frequency of each element and a list to store the frequency and corresponding elements.
