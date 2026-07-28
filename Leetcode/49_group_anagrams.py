3b6052bb9f0b95abb44c3c73d438733cccd19ee7
akash <akshdhr117@gmail.com>
2026-07-28 23:13:45 +0530

    revision

--- Leetcode/49_group_anagrams.py
+++ Leetcode/49_group_anagrams.py
@@ -1,13 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = {}
-        for s in strs:
-            temp = ''.join(sorted(list(s)))
-            if temp in anagrams:
-                anagrams[temp].append(s)
+        for i in strs:
+            tmp = ''.join(sorted(i))
+            if tmp in anagrams:
+                anagrams[tmp].append(i)
             else:
-                anagrams[temp] = [s]
-        return list(anagrams.values())
+                anagrams[tmp] = [i]
+        ans = []
+        for v in anagrams.values():
+            ans.append(v)
+        return ans
+        
         
 # time complexity: O(N * K log K) where N is the number of strings in the input array and K is the maximum length of a string in the input array. Sorting each string takes O(K log K) time and we do this for all N strings.
 # space complexity: O(N * K) in the worst case when all strings are anagrams of each other, we will have to store all strings in the hash map.
