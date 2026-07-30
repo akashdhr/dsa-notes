85b32fc8f948ec790395f44e487d340ba28e3330
akash <akshdhr117@gmail.com>
2026-07-30 23:24:38 +0530

    revision

--- Leetcode/20_valid_paranthesis.py
+++ Leetcode/20_valid_paranthesis.py
@@ -1,21 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         store = {
-            ']' : '[',
+            ')' : '(',
             '}' : '{',
-            ')' : '('
+            ']' : '['
         }
-
         stack = []
         for i in s:
-            if i not in store:
-                stack.append(i)
-            elif i in store:
-                if stack and stack[-1] == store[i]:
+            if stack and i in store:
+                if store[i] == stack[-1]:
                     stack.pop()
                 else:
                     return False
+            else:
+                stack.append(i)
         return stack == []
+        
     
 # time complexity: O(N) where N is the length of the string.
 # space complexity: O(N) in the worst case, we may store all the opening brackets in the stack.
