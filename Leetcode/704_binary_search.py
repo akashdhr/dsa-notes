666687eabaf59697a47fec86116a986c2fac07cb
akash <akshdhr117@gmail.com>
2026-07-31 20:34:00 +0530

    revision

--- Leetcode/704_binary_search.py
+++ Leetcode/704_binary_search.py
@@ -1,12 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums)-1
-        while l<=r:
+        while l <= r:
             m = (l+r)//2
-            if nums[m]==target:
-                return m
-            elif target < nums[m]:
-                r = m-1
+            if nums[m] == target:
+                return m 
+            elif nums[m] < target:
+                l = m + 1
             else:
-                l = m+1
+                r = m - 1
         return -1
+
+# time complexity: O(log N) where N is the number of elements in the input array since we are using binary search to find the target element.
+# space complexity: O(1) since we are using only a constant amount of extra space
