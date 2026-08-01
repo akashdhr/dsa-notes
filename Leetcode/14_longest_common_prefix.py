583b218b09a8cb288b49230fcc2a00b11ad0f970
akash <akshdhr117@gmail.com>
2026-08-01 23:41:05 +0530

    revision

--- Leetcode/14_longest_common_prefix.py
+++ Leetcode/14_longest_common_prefix.py
@@ -1,14 +1,15 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        base = strs[0]
-        res = ''
-        # for each item in base loop over all strings to check if the same is matching
-        for i in range(len(base)):
+        sample = strs[0]
+        ans = ''
+        for i in range(len(sample)):
             for s in strs[1:]:
-                if i >= len(s) or s[i] != base[i]:
-                    return res
-            res += base[i]
-        return res
+                if i == len(s) or sample[i] != s[i]:
+                    return ans
+            ans += sample[i]
+        return ans
+
+
                 
 # time complexity: O(S) where S is the sum of all characters in all strings. In the worst case, we compare each character of each string.
 # space complexity: O(1) if we don't consider the output string, otherwise O(M
