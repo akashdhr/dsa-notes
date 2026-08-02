3832dfec4083e58a55c65b12c331a95363bf1faf
akash <akshdhr117@gmail.com>
2026-08-02 17:42:07 +0530

    revision

--- Leetcode/169_majority_element.py
+++ Leetcode/169_majority_element.py
@@ -14,16 +14,16 @@ class Solution:
         # SC: O(1)
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        curr = nums[0]
+        val = nums[0]
         cnt = 0
         for i in nums:
             if cnt == 0:
-                curr = i
-            if i == curr:
+                val = i
+            if i == val:
                 cnt += 1
             else:
                 cnt -= 1
-        return curr
+        return val
 #time complexity: O(n) where n is the number of elements in the input array. We traverse the array once.
 #space complexity: O(1) since we are using only a constant amount of extra space to store the current candidate for majority element and its count.
 
