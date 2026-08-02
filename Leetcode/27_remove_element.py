36a65ed90e491bb4406169dfceb1852b0ce47f4d
akash <akshdhr117@gmail.com>
2026-08-02 17:39:30 +0530

    revision

--- Leetcode/27_remove_element.py
+++ Leetcode/27_remove_element.py
@@ -1,16 +1,20 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        l = 0
-        while l < len(nums) and nums[l] != val:
-            l += 1
-        r = l+1
-        while r < len(nums):
+        l = -1
+        for i in range(len(nums)):
+            if nums[i] == val:
+                l = i
+                break
+        if l == -1:
+            return len(nums)
+        i = l
+        for r in range(i+1, len(nums)):
             if nums[r] != val:
                 nums[l], nums[r] = nums[r], nums[l]
                 l += 1
-            r += 1
-        print(nums)
+        
         return l
+        
         
 
 # time complexity: O(N) where N is the length of the input array. We traverse the array once to find the first occurrence of val and then traverse it again to move the non-val elements to the left.
