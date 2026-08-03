7babdf0517fd72cdff61492b536dce4fbd98f194
akash <akshdhr117@gmail.com>
2026-08-03 09:24:02 +0530

    revision

--- Leetcode/344_reverse_string.py
+++ Leetcode/344_reverse_string.py
@@ -3,11 +3,16 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        l = 0
-        r = len(s)-1
-        while l <= r:
-            s[l], s[r] = s[r], s[l]
-            l += 1
-            r -= 1
+        low = 0
+        high = len(s) - 1
+
+        while low <= high:
+            s[low], s[high] = s[high], s[low]
+            low += 1
+            high -= 1
+        
         return s
+
+# time complexity: O(N) where N is the length of the string.
+# space complexity: O(1) since we are using constant space to store the pointers and temporary variables.
         
