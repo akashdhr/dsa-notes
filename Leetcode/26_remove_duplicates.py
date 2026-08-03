f19dd58b0079d4d612354e4be73742e8efe329a0
akash <akshdhr117@gmail.com>
2026-08-03 13:09:56 +0530

    revision

--- Leetcode/26_remove_duplicates.py
+++ Leetcode/26_remove_duplicates.py
@@ -1,13 +1,13 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        curr = 0
-        for fast in range(1, len(nums)):
-            if nums[fast] != nums[curr]:
-                curr += 1
-                nums[curr] = nums[fast]
-        return curr + 1
+        cur = nums[0]
+        ind = 1
+        for i in range(len(nums)):
+            if nums[i] != cur:
+                nums[ind] = nums[i]
+                cur = nums[i]
+                ind += 1
+        return ind
         
 #time complexity: O(n) where n is the number of elements in the array.
 #space complexity: O(1) as we are not using any extra space.
