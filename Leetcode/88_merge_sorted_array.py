2c7de24d8b5eeae038e218d405dc10f4e877617e
akash <akshdhr117@gmail.com>
2026-08-03 12:51:48 +0530

    revision and next time solve for optimal

--- Leetcode/88_merge_sorted_array.py
+++ Leetcode/88_merge_sorted_array.py
@@ -3,27 +3,31 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = m - 1 #last index of nums1 (excluding the blank values)
-        j = n - 1 #last index of nums2
-        k = m + n -1 #last index of final nums1
-
-        while i >= 0 and j >= 0:
-            if nums1[i] < nums2[j]:
-                nums1[k] = nums2[j]
-                k -= 1
-                j -= 1
+        ans = [0] * (m + n)
+        l1 = l2 = 0
+        k = 0
+        while l1 < m and l2 < n:
+            if nums1[l1] <= nums2[l2]:
+                ans[k] = nums1[l1]
+                k += 1
+                l1 += 1
             else:
-                nums1[k] = nums1[i]
-                k -= 1
-                i -= 1
+                ans[k] = nums2[l2]
+                k += 1
+                l2 += 1
+        while l1 < m:
+            ans[k] = nums1[l1]
+            l1 += 1
+            k += 1
+        while l2 < n:
+            ans[k] = nums2[l2]
+            l2 += 1
+            k += 1
+        for l in range(m + n):
+            nums1[l] = ans[l]
+        
 
-        #in case all nums1 exhausted and nums2 still pending
-        while j >= 0:
-            nums1[k] = nums2[j]
-            k -= 1
-            j -= 1
 
-        print(nums1)
 
 # time complexity: O(m + n) where m and n are the lengths of the input arrays nums1 and nums2 respectively. We traverse both arrays once to merge them.
 # space complexity: O(1) since we are modifying nums1 in-place and not using any additional data structures that grow with the input size.
