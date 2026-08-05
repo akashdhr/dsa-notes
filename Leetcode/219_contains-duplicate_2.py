f7d46ed0c8481bf720f56e68d6a5a522bee432be
akash <akshdhr117@gmail.com>
2026-08-05 22:16:59 +0530

    revision

--- Leetcode/219_contains-duplicate_2.py
+++ Leetcode/219_contains-duplicate_2.py
@@ -6,7 +6,21 @@ class Solution:
                 return True
             hashset[nums[i]] = i
         return False 
-        
+
+### Alternate approach
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        store = defaultdict(list)
+        for i in range(len(nums)):
+            store[nums[i]].append(i)
+        for v in store.values():
+            print(v)
+            if len(v) > 1:
+                for i in range(len(v)-1):
+                    if abs(v[i] - v[i+1]) <= k:
+                        return True
+        return False
 
 # Time complexity: O(N)
 # Space complexity: O(N)
