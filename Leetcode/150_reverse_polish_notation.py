f80075974d8854b3376805a3ec3023d2c0a09fad
akash <akshdhr117@gmail.com>
2026-08-05 22:45:35 +0530

    revision

--- Leetcode/150_reverse_polish_notation.py
+++ Leetcode/150_reverse_polish_notation.py
@@ -1,20 +1,22 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        for t in tokens:
-            if t in '*/-+':
-                b = stack.pop()
-                a = stack.pop()
-                if t == '+':
-                    stack.append(a + b)
-                elif t == '*':
-                    stack.append(a * b)
-                elif t == '-':
-                    stack.append(a - b)
-                elif t == '/':
-                    stack.append(int(a/b))
+        for i in tokens:
+            if i in '+-*/':
+                x = stack.pop()
+                y = stack.pop()
+                if i == '+':
+                    stack.append(x + y)
+                elif i == '*':
+                    stack.append(x * y)
+                elif i == '-':
+                    stack.append(y - x)
+                elif i == '/':
+                    stack.append(int(y/x))
             else:
-                stack.append(int(t))
+                stack.append(int(i))
         return stack[0]
+
+        
 #time complexity: O(N) where N is the number of tokens.
 #space complexity: O(N) in the worst case, we may store all the tokens in the stack.
