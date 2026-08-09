b6d1da6506eea55f536f134ad98c6e539e9de893
akash <akshdhr117@gmail.com>
2026-08-09 13:45:34 +0530

    revision

--- Leetcode/735_asteroid_collision.py
+++ Leetcode/735_asteroid_collision.py
@@ -1,20 +1,21 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []
-        for a in asteroids:
+        for i in asteroids:
             alive = True
-            while alive and stack and a < 0 and stack[-1] > 0:
-                if stack[-1] < abs(a):
+            while stack and i < 0 and stack[-1] > 0:
+                if stack[-1] < abs(i):
                     stack.pop()
-                elif stack[-1] == abs(a):
-                    stack.pop()
+                elif stack[-1] == abs(i):
                     alive = False
+                    stack.pop()
+                    break
                 else:
                     alive = False
+                    break
             if alive:
-                stack.append(a)
+                stack.append(i)
         return stack
-                
         
 #time complexity: O(N) where N is the number of asteroids.
 #space complexity: O(N) in the worst case when all asteroids are moving in the same direction and there are no collisions, we will have to store all asteroids in the stack.
