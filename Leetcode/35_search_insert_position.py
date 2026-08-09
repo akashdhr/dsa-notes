752782b391516b30704c87536b6a63b8a3a9ec14
akash <akshdhr117@gmail.com>
2026-08-09 14:05:50 +0530

    revision

--- Leetcode/35_search_insert_position.py
+++ Leetcode/35_search_insert_position.py
@@ -11,3 +11,29 @@ class Solution:
             else:
                 l = m+1
         return ans
+
+
+## Alternative Implementation
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        ans = len(nums)
+        if target > nums[-1]:
+            return len(nums)
+        elif target < nums[0]:
+            return 0
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                ans = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+        return ans + 1
+
+#time complexity: O(logN) where N is the number of elements in the array.
+#space complexity: O(1) as we are using constant space.
