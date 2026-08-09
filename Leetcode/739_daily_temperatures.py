1a2e63e50097a55c304026a24d788c4f0a94ef65
akash <akshdhr117@gmail.com>
2026-08-09 11:55:14 +0530

    try once more

--- Leetcode/739_daily_temperatures.py
+++ Leetcode/739_daily_temperatures.py
@@ -1,14 +1,17 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        answer = [0] * len(temperatures)
-        stack = []
-        for i in range(len(temperatures)):
-            while stack and temperatures[i] > temperatures[stack[-1]]:
-                diff = i - stack[-1]
-                answer[stack[-1]] = diff
-                stack.pop()
-            stack.append(i)
-        return answer
+        iwarm = [[temperatures[-1], len(temperatures)-1]] # stores the last warm temperature stack: [temp, index]
+        ans = [0] * len(temperatures)
+        # iterate from the back
+        for i in range(len(temperatures)-2, -1, -1):
+            while iwarm and temperatures[i] >= iwarm[-1][0]:
+                iwarm.pop()
+            if iwarm:
+                ans[i] = iwarm[-1][1] - i
+            iwarm.append([temperatures[i], i])
+        return ans
+            
+
 
 #time complexity: O(N) where N is the number of temperatures.   
 #space complexity: O(N) in the worst case, we may store all the temperatures in the stack.  
