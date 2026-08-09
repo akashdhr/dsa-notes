291663a4bd8726b9e5852811536edfa3ca31cc4d
akash <akshdhr117@gmail.com>
2026-08-09 15:50:21 +0530

    Search space logic was fine. Need to analyse more on which side to search on inside the BS algorithm.

--- Leetcode/875_koko_eating_bananas.py
+++ Leetcode/875_koko_eating_bananas.py
@@ -1,23 +1,26 @@
+import math
+
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def totalTime(piles, speed):
-            t = 0
+        def totalTime(s):
+            total = 0
             for i in piles:
-                t += math.ceil(i/speed)
-            return t
+                total += math.ceil(i/s)
+            return total
         
-        high = max(piles) # speed cannot be more than max bananas/pile in piles
         low = 1
-        res = -1
+        high = max(piles)
+        ans = high
         while low <= high:
             mid = (low + high)//2
-            if totalTime(piles, mid) <= h:
-                res = mid
+            timeTaken = totalTime(mid)
+            if timeTaken <= h:
+                ans = mid
                 high = mid - 1
             else:
                 low = mid + 1
-        return res
-
+        return ans
+        
 
 # time complexity: O(N log M) where N is the number of piles and M is the maximum bananas in a pile. why? because for each speed we are calculating total hours which takes O(N) time and we are doing binary search on speed which takes O(log M) time.
 # space complexity: O(1) as we are using only constant extra space.       
