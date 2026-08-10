a1d857253e9db56d1e8dfeed4a9af63fead9a273
akash <akshdhr117@gmail.com>
2026-08-10 18:45:41 +0530

    revision done

--- Leetcode/81_search_rotated_sorted_array.py
+++ Leetcode/81_search_rotated_sorted_array.py
@@ -23,6 +23,31 @@ class Solution:
                 else:
                     high = mid - 1
         return False
-               
+
+## Alternate Solution
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high)//2
+            if nums[mid] == target:
+                return True
+            while low < high and nums[low] == nums[mid] == nums[high]:
+                low += 1
+                high -= 1
+            if nums[low] <= nums[mid]:    
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return False
+                   
 #time complexity: O(log N) on average, O(N) in the worst case due to duplicates.
 #space complexity: O(1) 
