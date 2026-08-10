37ad04b4afbea93d7a088fbce8f495a3f53f0a6a
akash <akshdhr117@gmail.com>
2026-08-10 18:55:55 +0530

    revision

--- Leetcode/153_find_min_rotated_sorted_array.py
+++ Leetcode/153_find_min_rotated_sorted_array.py
@@ -1,28 +1,19 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        res = nums[0]
-        l, r = 0, len(nums)-1
-        while l<=r:
-            m = (l+r)//2
-            # Optimization Step 
-            # If array is already sorted then no need to perform BS on that space. Just return the minimum from the sorted half
-            if nums[l] <= nums[r]:
-                res = min(res, nums[l])
-                break
-            # Identify sorted half
-            if nums[l] <= nums[m]:
-                # Update the minimum value in res
-                # move l to m+1 and eliminate left sorted half
-                res = min(res, nums[l])
-                l = m+1
-            else:
-                # If right half is sorted
-                # Update the minimum value in res
-                # move h to m-1 and eliminate right sorted half
-                res = min(res, nums[m])
-                r = m-1
-        return res 
+        if nums[0] < nums[-1]:
+            return nums[0]
+        low = 0
+        high = len(nums) - 1
+        ans = nums[0]
+        while low <= high:
+            mid = (low + high)//2
+            if nums[low] <= nums[mid]:
+                ans = min(ans, nums[low])
+                low = mid + 1
+            elif nums[mid] < nums[high]:
+                ans = min(ans, nums[mid])
+                high = mid - 1
+        return ans
 
-
-            
-        
+# time complexity: O(logN) where N is the number of elements in the array.
+# space complexity: O(1) as we are using constant space.
