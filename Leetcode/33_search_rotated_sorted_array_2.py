b2cc945048dbfd8e7f1cedc8dda3118be95a186f
akash <akshdhr117@gmail.com>
2026-08-10 18:27:03 +0530

    revision

--- Leetcode/33_search_rotated_sorted_array_2.py
+++ Leetcode/33_search_rotated_sorted_array_2.py
@@ -1,25 +1,22 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> bool:
-        l, r = 0, len(nums)-1
-        while l<=r:
-            m = (l+r)//2
-            if nums[m] == target:
-                return True
-            # check edge case where nums[l] == nums[m] == nums[r]
-            if nums[l] == nums[m] and nums[m] == nums[r]:
-                l += 1
-                r -= 1
-                continue # continue does not execute the rest of the code to ensure the search space does not contain edge case
-            # Check if left half is sorted
-            if nums[l] <= nums[m]:
-                if nums[l] <= target and target <= nums[m]:
-                    r = m-1
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high)//2
+            if nums[mid] == target:
+                return mid
+            if nums[low] <= nums[mid]:    
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
                 else:
-                    l = m+1
-            # Check if right half is sorted
+                    low = mid + 1
             else:
-                if nums[m] <= target and target <= nums[r]:
-                    l = m+1
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
                 else:
-                    r = m-1
-        return False
+                    high = mid - 1
+        return -1
+            
+# time complexity: O(logN) where N is the number of elements in the array.
+# space complexity: O(1) as we are using constant space.
