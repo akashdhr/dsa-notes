3fb7859b56bbe407d67ee43ab3f7302ce96c558d
akash <akshdhr117@gmail.com>
2026-08-15 23:00:02 +0530

    need more practice on trees

--- Leetcode/543_diameter_binary_tree.py
+++ Leetcode/543_diameter_binary_tree.py
@@ -48,3 +48,28 @@ class Solution:
 
 # time complexity: O(N) where N is the number of nodes in the binary tree. We visit each node exactly once.
 # space complexity: O(H) where H is the height of the binary tree. This space is used by the recursion stack. In the worst case (skewed tree), H can be O(N). In a balanced tree, H is O(log N).
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        maxDiameter = 0
+        def dfs(node):
+            nonlocal maxDiameter
+            if not node:
+                return 0
+            leftDepth = dfs(node.left)
+            rightDepth = dfs(node.right)
+            diameter = leftDepth + rightDepth
+            maxDiameter = max(maxDiameter, diameter)
+
+            return 1 + max(leftDepth, rightDepth)
+        dfs(root)
+        return maxDiameter
+
+# time complexity: O(N) where N is the number of nodes in the binary tree. We visit each node exactly once.
+# space complexity: O(H) where H is the height of the binary tree. This space is used by the recursion stack. In the worst case (skewed tree), H can be O(N). In a balanced tree, H is O(log N).
