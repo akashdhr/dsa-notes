7fc21348ab5744ce4a07f2361fa58527f1ce6558
akash <akshdhr117@gmail.com>
2026-08-15 18:10:33 +0530

    revision

--- Leetcode/144_binary_tree_preorder.py
+++ Leetcode/144_binary_tree_preorder.py
@@ -8,14 +8,15 @@
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res = []
-        def dfs(node, res):
+        def dfs(res, node):
             if not node:
                 return
             res.append(node.val)
-            dfs(node.left, res)
-            dfs(node.right, res)
-        dfs(root, res)
+            dfs(res, node.left)
+            dfs(res, node.right)
+        dfs(res, root)
         return res
+        
         
 # time complexity: O(N) where N is the number of nodes in the binary tree. We visit each node exactly once.
 # space complexity: O(H) where H is the height of the binary tree. This space is used by the recursion stack. In the worst case (skewed tree), H can be O(N). In a balanced tree, H is O(log N).
