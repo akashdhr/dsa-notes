46cebdf70ffa8f8eb9bbec6c55a6e122bd557484
akash <akshdhr117@gmail.com>
2026-08-15 18:12:58 +0530

    revision

--- Leetcode/145_binary_tree_postorder.py
+++ Leetcode/145_binary_tree_postorder.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res = []
@@ -15,6 +16,6 @@ class Solution:
             res.append(node.val)
         dfs(res, root)
         return res
-
+        
 # time complexity: O(N) where N is the number of nodes in the binary tree. We visit each node exactly once.
 # space complexity: O(H) where H is the height of the binary tree. This space is used by the recursion stack. In the worst case (skewed tree), H can be O(N). In a balanced tree, H is O(log N).
