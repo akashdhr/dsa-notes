3e4d67e16807ad1b7aac786cf266959cb55bef3e
akash <akshdhr117@gmail.com>
2026-08-15 10:30:42 +0530

    needs revision

--- Leetcode/143_reorder_list.py
+++ Leetcode/143_reorder_list.py
@@ -8,32 +8,39 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # Find the middle
+        # break the list from the middle
+        # reverse the second half
+        # merge the two
+        
         slow = fast = head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-
-        # Reverse the second half
+        
         second = slow.next
-        slow.next = None # break the link
+        slow.next = None #break the link of second half from the first half
         prev = None
+        # reverse the second half
         while second:
             temp = second.next
             second.next = prev
             prev = second
             second = temp
-        
-        # Merge the list
+        # reversed second half is ready. time to merge with the first half
         first = head
         second = prev
+        # iterating on second because it will be shorter or equal to the first half
         while second:
             temp1 = first.next
             temp2 = second.next
-            first.next  = second
+            first.next = second
             second.next = temp1
             first = temp1
             second = temp2
+        
+        return head
 
+
+    
 #time complexity: O(n) where n is the number of nodes in the linked list.
 #space complexity: O(1) since we are modifying the list in place and not using any additional data structures to store the nodes.
